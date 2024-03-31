de3fdf36aa13e5fb22e16998492526c7a528ff0d
diniamo <diniamo69@gmail.com>
2024-03-31 18:11:26 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A collection of my self-maintained nix derivations";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux"];
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        mkPackage = path: pkgs.callPackage path {};
+        mkMpvScript = path: pkgs.mpvScripts.callPackage path {};
+      in {
+        formatter = pkgs.alejandra;
+
+        packages = {
+          bencode-pretty = mkPackage ./pkgs/bencode-pretty.nix;
+
+          mpvScripts = {
+            SimpleUndo = mkMpvScript ./pkgs/mpvScripts/SimpleUndo.nix;
+            skiptosilence = mkMpvScript ./pkgs/mpvScripts/skiptosilence.nix;
+          };
+        };
+      };
+    };
+}
