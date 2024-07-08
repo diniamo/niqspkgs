b02c7b1e3b1bf7f293a0046e4cb99375c4a995d0
diniamo <diniamo53@gmail.com>
2024-07-08 15:24:10 +0200

    direnv-nvim: bump

--- pkgs/vimPlugins/direnv-nvim.nix
+++ pkgs/vimPlugins/direnv-nvim.nix
@@ -4,11 +4,12 @@
 }:
 buildVimPlugin {
   pname = "direnv.nvim";
-  version = "2024-07-04";
+  version = "2024-07-08";
+
   src = fetchFromGitHub {
     owner = "NotAShelf";
     repo = "direnv.nvim";
-    rev = "f50828ef06274359ca0138e5b8952a3fa87209f4";
-    hash = "sha256-jXjKOPidmS+Y0DaBZxrUat8SyRX1uQntLCmN/EdH0iY=";
+    rev = "abdbf48f5665b8eaf54c07cc11fb8b3af96c41fd";
+    hash = "sha256-c6GHD3oZBEa44pCAMuELPoVXdv6GkAeRMVxnoqN3yAY=";
   };
 }
