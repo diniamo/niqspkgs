5b621aa3f233163d698bf405c0603ce219197a94
diniamo <diniamo53@gmail.com>
2024-07-11 14:32:09 +0200

    swayimg-git: bump

--- pkgs/swayimg-git.nix
+++ pkgs/swayimg-git.nix
@@ -3,12 +3,12 @@
   fetchFromGitHub,
 }:
 swayimg.overrideAttrs {
-  version = "2024-07-08";
+  version = "2024-07-11";
 
   src = fetchFromGitHub {
     owner = "artemsen";
     repo = "swayimg";
-    rev = "2f33c4c869139d234e81c535334cfc526ee9b481";
-    hash = "sha256-YmiT3W7VrTzH9sK3H2ownUHRiv8GeFbEqu1V/nJGyPs=";
+    rev = "c78d785128880dd736ebd175f18f32dcd928b2e2";
+    hash = "sha256-RIYpKQvZW+2mehnGhSjTHmwt6MtGlskMP0zWIJMWmCA=";
   };
 }
