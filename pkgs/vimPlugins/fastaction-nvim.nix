b1cc61eac0bb7363b62c72447cb197821c6ad51b
diniamo <diniamo53@gmail.com>
2024-07-19 14:04:13 +0200

    fastaction-nvim: bump

--- pkgs/vimPlugins/fastaction-nvim.nix
+++ pkgs/vimPlugins/fastaction-nvim.nix
@@ -9,7 +9,7 @@ buildVimPlugin {
   src = fetchFromGitHub {
     owner = "Chaitanyabsprip";
     repo = "fastaction.nvim";
-    rev = "17c9bd382aea14a0575b1e6c4d21c50cb8f30543";
-    hash = "sha256-MzFpD3c9WdLHhibOejZKIUFhCipX5ZWefCMO6LXOKkA=";
+    rev = "0912cb71c947f48c3b361cf28e3079e169fc0416";
+    hash = "sha256-MSmsbv2K8R2pUuEgSZYRWmDR808lIF285yRlDlV+3dw=";
   };
 }
