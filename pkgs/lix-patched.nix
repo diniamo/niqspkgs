8e3db8b71c4b27ce79b3347aa90a830798e54e3e
diniamo <diniamo53@gmail.com>
2024-08-23 00:44:05 +0200

    lix-patched: update to git, update default-flake patch

--- pkgs/lix-patched.nix
+++ pkgs/lix-patched.nix
@@ -1,11 +1,15 @@
-{lix}:
-lix.overrideAttrs (prev: {
-  pname = "lix-patched";
+{system}: let
+  flake = builtins.getFlake "github:lix-project/lix/f2e7f8bab875809e8b489e1e5a7aa8572bb4bc13";
 
-  patches =
-    (prev.patches or [])
-    ++ [
-      ./patches/lix-default-flake.patch
-      ./patches/lix-nix3-shell.patch
-    ];
-})
+  package = flake.packages.${system}.default;
+in
+  package.overrideAttrs (prev: {
+    pname = "lix-patched";
+
+    patches =
+      (prev.patches or [])
+      ++ [
+        ./patches/lix-default-flake.patch
+        ./patches/lix-nix3-shell.patch
+      ];
+  })
