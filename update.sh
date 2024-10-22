934a49bfd8199629246dd70e806e5a2149627d5e
diniamo <diniamo53@gmail.com>
2024-10-22 15:50:45 +0200

    update.sh: update version, commit file

--- update.sh
+++ update.sh
@@ -1,33 +1,45 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -p dash nix-prefetch jaq alejandra -i dash
+#!nix-shell -p dash nix-prefetch jaq alejandra curl git -i dash
 
 set -e
 
 for pkg in "$@"; do
-    if [ ! -f "$pkg" ]; then
-        for file in $(find "$(dirname "$0")" -name '*.nix'); do
-            if [ "$(basename "$file")" = "$pkg.nix" ]; then
-                pkg="$file"
-                break
-            fi
-        done
+    if [ -f "$pkg" ]; then
+        file="$pkg"
+
+        name="${pkg##*/}"
+        name="${name%.*}"
+    else
+        file="$(find "${0%/*}" -name "$pkg.nix")"
+        if [ -n "$file" ]; then
+            # file="$file"
+            name="$pkg"
+        else
+            echo "Couldn't find package $pkg!" 1>&2
+            continue
+        fi
     fi
 
-    if [ ! -f "$pkg" ]; then
-        echo "Couldn't find package $pkg!" 1>&2
-        continue
-    fi
+    alejandra --quiet "$file"
+    content="$(cat "$file")"
 
-    alejandra --quiet "$pkg"
-
-    fetcher="$(grep -oP 'src = \K\w+' "$pkg")"
-    owner="$(grep -oP 'owner = "\K[^"]+' "$pkg")"
-    repo="$(grep -oP 'repo = "\K[^"]+' "$pkg")"
+    version="$(printf '%s' "$content" | grep -oP 'version = "\K[^"]+')"
+    fetcher="$(printf '%s' "$content" | grep -oP 'src = \K\w+')"
+    owner="$(printf '%s' "$content" | grep -oP 'owner = "\K[^"]+')"
+    repo="$(printf '%s' "$content" | grep -oP 'repo = "\K[^"]+')"
 
     json="$(nix-prefetch "$fetcher" --owner "$owner" --repo "$repo" --quiet --output json)"
 
+    commit_hash="$(printf '%s' "$json" | jaq -r '.rev')"
+    date="$(date --date="$(curl -L "https://api.github.com/repos/$owner/$repo/commits/$commit_hash" | jaq -r '.commit.committer.date')" +%Y-%m-%d)"
+
     sed -i \
-        -e "s/rev = \".*\"/rev = \"$(printf '%s' "$json" | jaq -r '.rev')\"/" \
+        -e "s/version = \".*\"/version = \"$date\"/" \
+        -e "s/rev = \".*\"/rev = \"$commit_hash\"/" \
         -e "s/hash = \".*\"/hash = \"$(printf '%s' "$json" | jaq -r '.sha256' | sed 's,/,\\/,')\"/" \
-        "$pkg"
+        "$file"
+
+    git reset # Unstage everything
+    git add "$file"
+    git commit --all --message="$name: $version -> $date"
 done
