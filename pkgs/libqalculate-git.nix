1793b26118503da244625353487601cef03994ef
diniamo <diniamo53@gmail.com>
2024-10-22 16:54:13 +0200

    libqalculate-git: 2024-10-21 -> 2024-10-22

--- pkgs/libqalculate-git.nix
+++ pkgs/libqalculate-git.nix
@@ -3,12 +3,12 @@
   fetchFromGitHub,
 }:
 libqalculate.overrideAttrs {
-  version = "2024-10-21";
+  version = "2024-10-22";
 
   src = fetchFromGitHub {
     owner = "Qalculate";
     repo = "libqalculate";
-    rev = "52037dd0613053b44c7178710e35d864dc84b8c8";
-    hash = "sha256-SVbPXJEbrXBS6jQqJeAI380ZViKKuUDhUP98BtgzQSQ=";
+    rev = "d3035a023e47e60f9a3621083cae9a3fc15ab0c6";
+    hash = "sha256-AIynupy5cBiZ8TzRCgV3w91+kTm8ZlY0w+6VGAArjWU=";
   };
 }
