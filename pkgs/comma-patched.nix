d479ca746c41c0c6303c4c6b05ea98db0170d24d
diniamo <diniamo53@gmail.com>
2024-11-15 21:05:45 +0100

    comma-patched: cache-choices-sensible-print -> 2024-11-15

--- pkgs/comma-patched.nix
+++ pkgs/comma-patched.nix
@@ -4,16 +4,17 @@
 }:
 comma.overrideAttrs (prev: rec {
   pname = "comma-patched";
+  version = "2024-11-15";
 
   src = fetchFromGitHub {
-    owner = "diniamo";
+    owner = "nix-community";
     repo = "comma";
-    rev = "cache-choices-sensible-print";
-    hash = "sha256-mtlW9wDZyISjMYZeVSUGDbBCEzu4+2neDx3fD9rj0To=";
+    rev = "ae61728cb99fc680bae716660532d11c017ef956";
+    hash = "sha256-UPqArYDOWMe8Bhs7tKVSdtvYbAZY4u0ZV85WfYQ16vY=";
   };
 
   cargoDeps = prev.cargoDeps.overrideAttrs {
     inherit src;
-    outputHash = "sha256-gjChTXqQAZiTlaAR8ibLuMBXTFCcaZCA2riLZa5JoRU=";
+    outputHash = "sha256-poQDzC5DLkwLMwt5ieZCSyrQIKkuYq6hu6cj7lcDb4c=";
   };
 })
