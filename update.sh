ef54584820c0152df7f7ae0bd29d112a1eb7b8fb
diniamo <diniamo53@gmail.com>
2024-11-15 21:05:18 +0100

    update.sh: fix

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -p dash nix-prefetch jaq alejandra curl git -i dash
 
-set -e
+set -ex
+
+root="$(dirname "$0")"
 
 for pkg in "$@"; do
     if [ -f "$pkg" ]; then
@@ -10,12 +12,11 @@ for pkg in "$@"; do
         name="${pkg##*/}"
         name="${name%.*}"
     else
-        file="$(find "${0%/*}" -name "$pkg.nix")"
+        file="$(find "$root" -name "$pkg.nix")"
         if [ -n "$file" ]; then
-            # file="$file"
             name="$pkg"
         else
-            echo "Couldn't find package $pkg!" 1>&2
+            printf "Couldn't find package %s." "$pkg" 1>&2
             continue
         fi
     fi
@@ -27,10 +28,15 @@ for pkg in "$@"; do
     fetcher="$(printf '%s' "$content" | grep -oP 'src = \K\w+')"
     owner="$(printf '%s' "$content" | grep -oP 'owner = "\K[^"]+')"
     repo="$(printf '%s' "$content" | grep -oP 'repo = "\K[^"]+')"
+    rev="$(printf '%s' "$content" | grep -oP 'rev = "\K[^"]+')"
 
     json="$(nix-prefetch "$fetcher" --owner "$owner" --repo "$repo" --quiet --output json)"
 
     commit_hash="$(printf '%s' "$json" | jaq -r '.rev')"
+    if [ "$rev" = "$commit_hash" ]; then
+        printf '%s is already up to date.' "$pkg" 1>&2
+        continue
+    fi
     date="$(date --date="$(curl -L "https://api.github.com/repos/$owner/$repo/commits/$commit_hash" | jaq -r '.commit.committer.date')" +%Y-%m-%d)"
 
     sed -i \
