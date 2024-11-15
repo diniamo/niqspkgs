604432b53f80f5a909339b76d39586d30a906db6
diniamo <diniamo53@gmail.com>
2024-11-15 21:08:18 +0100

    nh-patched: 3.5.26 -> 2024-11-15

--- pkgs/nh-patched.nix
+++ pkgs/nh-patched.nix
@@ -2,7 +2,24 @@
   nh,
   self,
   stdenv,
+  fetchFromGitHub,
 }:
-nh.override {
+(nh.override {
   nix-output-monitor = self.packages.${stdenv.system}.nom-patched;
-}
+})
+.overrideAttrs (prev: rec {
+  pname = "nh-patched";
+  version = "2024-11-15";
+
+  src = fetchFromGitHub {
+    owner = "viperML";
+    repo = "nh";
+    rev = "ed2116065353603859852efbff5702489d965684";
+    hash = "sha256-k8rz5RF1qi7RXzQYWGbw5pJRNRFIdX85SIYN+IHiVL4=";
+  };
+
+  cargoDeps = prev.cargoDeps.overrideAttrs {
+    inherit src;
+    outputHash = "sha256-1Yu6C96AJRqwMgzIght9DnteO5WKzMFQfgil0lTWuiI=";
+  };
+})
