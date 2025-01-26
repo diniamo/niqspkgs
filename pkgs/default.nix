a5ae170486957cabfde95453982bf813bfa5360d
diniamo <diniamo53@gmail.com>
2025-01-26 13:17:53 +0100

    pkgs: remove unused/broken packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -10,8 +10,6 @@
     mkMpvScript = path: pkgs.mpvScripts.callPackage path {};
 
     packages = {
-      starship-patched = mkPackage ./starship-patched.nix;
-      alacritty-sixel = mkPackage ./alacritty-sixel.nix;
       swayimg-git = mkPackage ./swayimg-git.nix;
       lix-patched = callPackage ./lix-patched.nix {inherit inputs;};
       comma-patched = mkPackage ./comma-patched.nix;
@@ -20,7 +18,6 @@
       fish-patched = callPackage ./fish-patched.nix {fish = packages.fish-git;};
       bibata-hyprcursor = mkPackage ./bibata-hyprcursor;
       coreutils-full-patched = mkPackage ./coreutils-full-patched.nix;
-      sway-unwrapped-git = mkPackage ./sway-unwrapped-git;
       fish-git = mkPackage ./fish-git.nix;
       jellyfin-intro-skipper = mkPackage ./jellyfin-intro-skipper.nix;
       file-roller-gtk3 = mkPackage ./file-roller-gtk3.nix;
--- pkgs/starship-patched.nix
+++
@@ -1,6 +0,0 @@
-{starship}:
-starship.overrideAttrs (prev: {
-  pname = "starship-patched";
-
-  patches = (prev.patches or []) ++ [./patches/starship-nix3-shell.patch];
-})
