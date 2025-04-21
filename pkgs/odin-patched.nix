0001dc6aa820c2d278ce6d53c7dcf2b29ee4cc3d
diniamo <diniamo53@gmail.com>
2025-04-21 11:34:22 +0200

    odin-patched: 2025-1 -> dev-2025-04

--- pkgs/odin-patched.nix
+++ pkgs/odin-patched.nix
@@ -9,16 +9,18 @@
 in
   stdenv.mkDerivation {
     pname = "odin";
-    version = "2025-01";
+    version = "dev-2025-04";
 
     src = fetchFromGitHub {
       owner = "odin-lang";
       repo = "Odin";
-      rev = "dev-2025-01";
-      hash = "sha256-GXea4+OIFyAhTqmDh2q+ewTUqI92ikOsa2s83UH2r58=";
+      tag = "dev-2025-04";
+      hash = "sha256-dVC7MgaNdgKy3X9OE5ZcNCPnuDwqXszX9iAoUglfz2k=";
     };
 
-    patches = [./patches/odin-system-raylib.patch];
+    patches = [
+      ./patches/odin-system-raylib.patch
+    ];
 
     dontConfigure = true;
     LLVM_CONFIG = "${llvmPackages.llvm.dev}/bin/llvm-config";
@@ -30,9 +32,7 @@ in
       which
     ];
 
-    postPatch = ''
-      patchShebangs build_odin.sh
-    '';
+    postPatch = "patchShebangs build_odin.sh";
 
     installPhase = ''
       runHook preInstall
@@ -45,6 +45,8 @@ in
 
       mkdir -p $out/share/vendor/raylib
       cp -r vendor/raylib/{rlgl,*.odin} $out/share/vendor/raylib
+
+      cp -r vendor/sdl3 $out/share/vendor
 
       # make -C "$out/share/vendor/cgltf/src/"
       # make -C "$out/share/vendor/stb/src/"
