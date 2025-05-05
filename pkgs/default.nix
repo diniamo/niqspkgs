9e438627b215493a9a3e079a32a4ccd212e6afbc
diniamo <diniamo53@gmail.com>
2025-05-05 10:59:10 +0200

    nh-patched: remove
    I stopped using nh for rebuilding since I wrote my own script. I also
    want to get rid of it for cleaning, but I haven't written a
    replacement for that yet.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -14,7 +14,6 @@
       lix-patched = callPackage ./lix-patched.nix {inherit inputs;};
       comma-patched = mkPackage ./comma-patched.nix;
       nom-patched = mkPackage ./nom-patched.nix;
-      nh-patched = callPackage ./nh-patched.nix {inherit self;};
       fish-patched = mkPackage ./fish-patched.nix;
       bibata-hyprcursor = mkPackage ./bibata-hyprcursor;
       file-roller-gtk3 = mkPackage ./file-roller-gtk3.nix;
