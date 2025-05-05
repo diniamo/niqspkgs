d752fd4bfbdcc76b8f09ff9f4c2107186dfe9309
diniamo <diniamo53@gmail.com>
2025-05-05 10:55:15 +0200

    comma-patched: self-reference instead of rec, use importCargoLock

--- pkgs/comma-patched.nix
+++ pkgs/comma-patched.nix
@@ -1,20 +1,21 @@
 {
   comma,
   fetchFromGitHub,
-}:
-comma.overrideAttrs (prev: rec {
-  pname = "comma-patched";
-  version = "2024-11-15";
+  rustPlatform
+}: let
+  final = comma.overrideAttrs (prev: {
+    pname = "comma-patched";
+    version = "2024-11-15";
 
-  src = fetchFromGitHub {
-    owner = "nix-community";
-    repo = "comma";
-    rev = "ae61728cb99fc680bae716660532d11c017ef956";
-    hash = "sha256-UPqArYDOWMe8Bhs7tKVSdtvYbAZY4u0ZV85WfYQ16vY=";
-  };
+    src = fetchFromGitHub {
+      owner = "nix-community";
+      repo = "comma";
+      rev = "ae61728cb99fc680bae716660532d11c017ef956";
+      hash = "sha256-UPqArYDOWMe8Bhs7tKVSdtvYbAZY4u0ZV85WfYQ16vY=";
+    };
 
-  cargoDeps = prev.cargoDeps.overrideAttrs {
-    inherit src;
-    outputHash = "sha256-poQDzC5DLkwLMwt5ieZCSyrQIKkuYq6hu6cj7lcDb4c=";
-  };
-})
+    cargoDeps = rustPlatform.importCargoLock {
+      lockFile = "${final.src}/Cargo.lock";
+    };
+  });
+in final
