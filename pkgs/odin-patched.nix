0c4ad2c1d2edabe43c9a1543c23785855a004891
diniamo <diniamo53@gmail.com>
2025-05-21 19:07:02 +0200

    odin-patched: dev-2025-01 -> 0-unstable-a2c0720

--- pkgs/odin-patched.nix
+++ pkgs/odin-patched.nix
@@ -1,5 +1,13 @@
-{odin}: odin.overrideAttrs (prev: {
+{odin, fetchFromGitHub}: odin.overrideAttrs (prev: {
   pname = "odin-patched";
+  version = "0-unstable-a2c0720";
+
+  src = fetchFromGitHub {
+    owner = "odin-lang";
+    repo = "Odin";
+    rev = "a2c0720fb046187bb00f5f3beeab4e9c284b18f0";
+    hash = "sha256-T3CCey2ql2qI8TJVzpxp7cS1wXy2CCGRmvFSmt2GQos=";
+  };
   
   patches = (prev.patches or []) ++ [
     ./patches/odin-system-raylib.patch
