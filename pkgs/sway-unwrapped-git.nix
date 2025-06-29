2c570a37d95b1b989fca041d496d2fc05f963b49
diniamo <diniamo53@gmail.com>
2025-06-29 14:06:42 +0200

    sway-unwrapped-git: 0-unstable-3d6b9a2 -> 0-unstable-56f2db0

--- pkgs/sway-unwrapped-git.nix
+++ pkgs/sway-unwrapped-git.nix
@@ -1,22 +1,22 @@
 {sway-unwrapped, wlroots, fetchFromGitLab, fetchFromGitHub}: (sway-unwrapped.override {
   wlroots = wlroots.overrideAttrs {
-    version = "0-unstable-8c7041c";
+    version = "0-unstable-48bd183";
     
     src = fetchFromGitLab {
       domain = "gitlab.freedesktop.org";
       owner = "wlroots";
       repo = "wlroots";
-      rev = "8c7041c4e842c9fb029a6371eb53f73aa98e7b31";
-      hash = "sha256-JXbtW7qcuX1Nvq4KBRRVx01IZA/T1LD7An5tKrNFIBg=";
+      rev = "48bd1831feff89ac94aacc2218601c2b569ef70c";
+      hash = "sha256-Y9wJQ2hhUH/F6p/Fsi+cwjVl8dD7YKnYNMZeTYwAMog=";
     };
   };
 }).overrideAttrs {
-  version = "0-unstable-3d6b9a2";
+  version = "0-unstable-56f2db0";
   
   src = fetchFromGitHub {
     owner = "swaywm";
     repo = "sway";
-    rev = "3d6b9a28480a398e3af869d4051181f98a042022";
-    hash = "sha256-Vq8wb+MqIpevbPwhvdEpwWB3XOth2VCnbEUBgUt5sBQ=";
+    rev = "56f2db062daa64a0df77a83823d85fec7c3a9f46";
+    hash = "sha256-jwV8dGsALPSAeWXJLBgIXERbHp7POduaFE1rPU2ofeg=";
   };
 }
