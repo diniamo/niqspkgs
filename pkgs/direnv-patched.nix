b1063c19e971eb4a7868caa0aa5fad415e87fe2a
diniamo <diniamo53@gmail.com>
2025-08-09 06:09:25 +0200

    direnv-patched: init at 2.37.1-unstable-c0ce70d

---
+++ pkgs/direnv-patched.nix
@@ -0,0 +1,10 @@
+{direnv, fetchFromGitHub}: direnv.overrideAttrs {
+  version = "2.37.1-unstable-c0ce70d";
+
+  src = fetchFromGitHub {
+    owner = "diniamo";
+    repo = "direnv";
+    rev = "c0ce70d6af65726fe08520d8dbb48f8d2ba62e29";
+    hash = "sha256-3fzLq8dvvmZGAMPf5ONOw/9ZIuQjN4euogQ++voSd+E=";
+  };
+}
